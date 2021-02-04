541391a94caa2d62709d6273e11af1995b1c03eb
zce <w@zce.me>
2021-02-04 12:50:17 +0800

    feat: update template config

--- index.js
+++ index.js
@@ -55,16 +55,6 @@ module.exports = {
       initial: 'caz-templates'
     },
     {
-      name: 'features',
-      type: 'multiselect',
-      message: 'Choose the features you need',
-      instructions: false,
-      choices: [
-        { title: 'Foo', value: 'foo' },
-        { title: 'Bar', value: 'bar', selected: true }
-      ]
-    },
-    {
       name: 'install',
       type: 'confirm',
       message: 'Install dependencies',
@@ -82,34 +72,14 @@ module.exports = {
       ]
     }
   ],
-  filters: {
-    // TODO: custom template filters
-    /** @param {{ features: string[] }} answers */
-    'test/**': answers => answers.features.includes('test')
-  },
-  helpers: {
-    // TODO: custom template helpers
-    upper: input => input.toUpperCase()
-  },
   // TODO: enable install by npm / yarn
   install: 'npm',
   // TODO: enable git init
   init: true,
   setup: async ctx => {
-    // TODO: custom template setup hook, execute after template loaded & inquire completed.
-    console.log('setup', ctx)
     ctx.config.install = ctx.answers.install && ctx.answers.pm
   },
-  prepare: async ctx => {
-    // TODO: custom template prepare hook, execute after template files prepare, before rename & render.
-    console.log('prepare', ctx)
-  },
-  emit: async ctx => {
-    // TODO: custom template emit hook, execute after all files emit to the destination.
-    console.log('emit', ctx)
-  },
   complete: async ctx => {
-    // TODO: custom complete callback
     console.clear()
     console.log(chalk`Created a new project in {cyan ${ctx.project}} by the {blue ${ctx.template}} template.\n`)
     console.log('Getting Started:')
