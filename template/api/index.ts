0f8b173982b97084aa57b9e1f7b8bfb7dc9d09d4
zce <w@zce.me>
2021-03-04 16:42:29 +0800

    feat: initial commit

--- template/api/index.ts
+++ template/api/index.ts
@@ -4,7 +4,7 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<any> => 
   const { name, host = 'zce.me' } = req.query // from querystring or pathinfo
   // const { name, host = 'zce.me' } = req.body // from request body
 
-  if (typeof name !== 'string' || name === '') {
+  if (name == null || name === '') {
     // return if without redirect url.
     return res.status(400).send({ message: 'Bad Request' })
   }
